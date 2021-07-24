8f6d76469a399667c14a910612e3f693d5411ebb
kiattisak tawaingnan <59010153@kmitl.ac.th>
2021-07-24 14:46:32 +0700

    Demo Sigleton

---
+++ restapi/src/main/java/com/example/restapi/GenerateIdService.java
@@ -0,0 +1,12 @@
+package com.example.restapi;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenerateIdService {
+
+    public String getId() {
+        return "XYZ7";
+    }
+
+}
